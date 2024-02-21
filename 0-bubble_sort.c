382fff57b2a70ee445d9e515eee7153a8414586b
Boluwatife Fasina <boluayofashina@gmail.com>
2024-02-20 17:45:00 -0800

    Task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * swap - Swap two integers in an array.
+ * @first: Pointer to the first integer to swap.
+ * @second: Pointer to the second integer to swap.
+ */
+void swap(int *first, int *second)
+{
+	 int temp;
+
+	 temp = *first;
+	 *first = *second;
+	 *second = temp;
+}
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using algorithm.
+ * @array: array of integers to sort.
+ * @size: Size of the array.
+ *
+ * Description: This function sorts the given array in ascending order
+ *              and prints the array after each swap operation.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	 size_t i, len = size;
+	 boolean swapped = false_value;
+
+	 if (array == NULL || size < 2)
+		  return;
+
+	 while (swapped == false_value)
+	 {
+		  swapped = true_value;
+		  for (i = 0; i < len - 1; i++)
+		  {
+			   if (array[i] > array[i + 1])
+			   {
+				    swap(array + i, array + i + 1);
+				    print_array(array, size);
+				    swapped = false_value;
+			   }
+		  }
+		  len--;
+	 }
+}
---
+++ sort.h
@@ -0,0 +1,42 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * enum boolean - Enumeration of Boolean values.
+ * @false_value: Represents a false boolean value, equals 0.
+ * @true_value: Represents a true boolean value, equals 1.
+ */
+typedef enum boolean
+{
+    false_value = 0,
+    true_value
+} boolean;
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Printing helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Sorting algoritms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif /* SORT_H */
